0f7a59a52d0728a610cd2c6c34a3e2314bb366ba
Reece Pritchard <rapritchard96@gmail.com>
2018-09-25 19:37:40 +0100

    Completed basic functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hangman
+
+This is a little project made to mess around with JavaScript OOP.
---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const puzzleElement = document.querySelector("#puzzle-word")
+const guessElement = document.querySelector("#guess-count")
+
+const renderGame = function(game){
+    puzzleElement.textContent = ""
+    puzzleElement.textContent = game.getPuzzle()
+    guessElement.textContent = game.currentStatus()
+}
+
+const game = new Hangman("Cat", 2)
+renderGame(game)
+
+window.addEventListener("keypress", function(e){
+    const guess = String.fromCharCode(e.charCode)
+    game.makeGuess(guess)
+    game.calculateStatus()
+    renderGame(game)
+})
