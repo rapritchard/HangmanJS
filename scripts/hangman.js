7d33a35a17e399facf9332b620e198c8d590a0ee
Reece Pritchard <rapritchard96@gmail.com>
2018-09-26 20:32:53 +0100

    Refactored hangman.js

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,59 +1,64 @@
 'use strict'
 
-const Hangman = function(wordToGuess, attempts){
-    this.wordToGuess = wordToGuess.toLowerCase().split("")
-    this.attempts = attempts
-    this.guessedLetters = []
-    this.status = "playing"
-}
+class Hangman{
+    constructor(wordToGuess, attempts) {
+        this.wordToGuess = wordToGuess.toLowerCase().split("")
+        this.attempts = attempts
+        this.guessedLetters = []
+        this.status = "playing"
+    }
 
-Hangman.prototype.getPuzzle = function(){
-    let hiddenWord = ""
+    // Get the current puzzle progress
+    getPuzzle(){
+        let hiddenWord = ""
 
-    this.wordToGuess.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === " "){
-            hiddenWord += letter
-        }else{
-            hiddenWord += "*"
-        }
-    })
-    return hiddenWord
-}
+        this.wordToGuess.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === " "){
+                hiddenWord += letter
+            }else{
+                hiddenWord += "*"
+            }
+        })
+        return hiddenWord
+    }
 
-Hangman.prototype.makeGuess = function(guess){
-    if(this.status === "playing")
-    {
-        guess = guess.toLowerCase()
-        // If the guess is unique
-        if(!this.guessedLetters.includes(guess)){
-            this.guessedLetters.push(guess)
-            // If the unique guess is not part of the word to guess, reduce attempts
-            if(!this.wordToGuess.includes(guess)){
-                this.attempts--
+    // Let the user make a guess
+    makeGuess(guess){
+        if(this.status === "playing")
+        {
+            guess = guess.toLowerCase()
+            // If the guess is unique
+            if(!this.guessedLetters.includes(guess)){
+                this.guessedLetters.push(guess)
+                // If the unique guess is not part of the word to guess, reduce attempts
+                if(!this.wordToGuess.includes(guess)){
+                    this.attempts--
+                }
             }
         }
     }
 
-}
+    // Determines current game status
+    calculateStatus(){
+        let finished = this.wordToGuess.every((letter) => this.guessedLetters.includes(letter))
 
-Hangman.prototype.calculateStatus = function(){
-    let finished = this.wordToGuess.every((letter) => this.guessedLetters.includes(letter))
-
-    if(this.attempts === 0){
-        this.status = "failed"
-    }else if(finished){
-        this.status = "finished"
-    }else{
-        this.status = "playing"
+        if(this.attempts === 0){
+            this.status = "failed"
+        }else if(finished){
+            this.status = "finished"
+        }else{
+            this.status = "playing"
+        }
     }
-}
 
-Hangman.prototype.currentStatus = function(){
-    if(this.status === "failed"){
-        return `Nice try! The word was "${this.wordToGuess.join("")}".`
-    }else if(this.status === "finished"){
-        return "Great work! You guess the word."
-    }else{
-        return `Guesses left: ${this.attempts}`
+    // Updates the current status message
+    currentStatus(){
+        if(this.status === "failed"){
+            return `Nice try! The word was "${this.wordToGuess.join("")}".`
+        }else if(this.status === "finished"){
+            return "Great work! You guess the word."
+        }else{
+            return `Guesses left: ${this.attempts}`
+        }
     }
 }
