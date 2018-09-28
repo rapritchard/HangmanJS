bdcbef322bd67411eaf0519b1fcd16a84211f538
Reece Pritchard <rapritchard96@gmail.com>
2018-09-28 13:57:45 +0100

    Switched to Fetch API

--- scripts/app.js
+++ scripts/app.js
@@ -23,16 +23,26 @@ window.addEventListener("keypress", (e) =>{
     renderGame(game)
 })
 
-
-
 getPuzzle("4").then((puzzle) =>{
     console.log(puzzle)
-}, (err) => {
+}).catch((err) => {
     console.log(`Error: ${err}`)
 })
 
 getCountry("GB").then((country) =>{
-    console.log(country)
-}, (err) => {
+    console.log(country.name)
+}).catch((err) => {
     console.log(`Error: ${err}`)
 })
+
+// fetch("http://puzzle.mead.io/puzzle", {}).then((response) =>{
+//     if(response.status === 200){
+//         return response.json()
+//     }else{
+//         throw new Error("Unable to fetch puzzle")
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error)
+// })
--- scripts/requests.js
+++ scripts/requests.js
@@ -1,35 +1,25 @@
 'use strict'
 
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
-
-    request.addEventListener("readystatechange", (e) => {
-        if(e.target.readyState === 4 && e.target.status === 200){
-            const data = JSON.parse(e.target.responseText)
-            resolve(data.puzzle)
-        }else if(e.target.readyState === 4){
-            reject("An error has taken place")
+const getPuzzle = (wordCount) => {
+    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+        if(response.status === 200){
+            return response.json()
+        }else{
+            throw new Error("Unable to fetch puzzle.")
         }
+    }).then((data) => {
+        return data.puzzle
     })
+}
 
-    request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    request.send()
-})
-
-const getCountry = (countryCode) => new Promise((resolve, reject) => {
-    countryCode = countryCode.toUpperCase()
-    const request = new XMLHttpRequest()
-
-    request.addEventListener("readystatechange", (e) =>{
-        if(e.target.readyState === 4 && e.target.status === 200){
-            const data = JSON.parse(e.target.responseText)
-            const countryData = data.find((country) => country.alpha2Code === countryCode)
-            resolve(countryData.name)
-        }else if(e.target.readyState === 4){
-            reject("An error has taken place")
+const getCountry = (countryCode) => {
+    return fetch("http://restcountries.eu/rest/v2/all").then((response) => {
+        if(response.status === 200){
+            return response.json()
+        }else{
+            throw new Error("Unable to fetch countries.")
         }
+    }).then((data) =>{
+        return data.find((country) => country.alpha2Code === countryCode)
     })
-
-    request.open("GET", "http://restcountries.eu/rest/v2/all")
-    request.send()
-})
+}
