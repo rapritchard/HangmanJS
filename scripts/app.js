e2ab362c447a0095144fb980432dbd62161c3271
Reece Pritchard <rapritchard96@gmail.com>
2018-09-28 16:47:13 +0100

    Added startgame() function

--- scripts/app.js
+++ scripts/app.js
@@ -7,30 +7,38 @@
 const puzzleElement = document.querySelector("#puzzle-word")
 const guessElement = document.querySelector("#guess-count")
 
-const renderGame = (game) => {
-    puzzleElement.textContent = ""
-    puzzleElement.textContent = game.puzzle
-    guessElement.textContent = game.statusMessage
-}
-
-const game = new Hangman("Car Parts", 2)
-renderGame(game)
+let game
 
 window.addEventListener("keypress", (e) =>{
     const guess = String.fromCharCode(e.charCode)
     game.makeGuess(guess)
     game.calculateStatus()
-    renderGame(game)
+    renderGame()
 })
 
-getPuzzle("3").then((puzzle) =>{
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
+const renderGame = () => {
+    puzzleElement.textContent = game.puzzle
+    guessElement.textContent = game.statusMessage
+}
 
-getCurrentCountry().then((country) => {
-    console.log(country.name)
-}).catch((err) => {
-    console.log(err)
-})
+const startGame = async () => {
+    const puzzle = await getPuzzle("2")
+    game = new Hangman(puzzle, 5)
+    renderGame()
+}
+
+document.querySelector("#reset").addEventListener("click", startGame)
+
+startGame()
+
+// getPuzzle("3").then((puzzle) =>{
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name)
+// }).catch((err) => {
+//     console.log(err)
+// })
